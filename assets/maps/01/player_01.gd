d634ea5b20a946bfa1b8491e15219e4fa695386c
fireb0y <58452997+fireBoyofficial@users.noreply.github.com>
2026-03-11 16:06:54 -0600

    movement for 01 garvie

---
+++ assets/maps/01/player_01.gd
@@ -0,0 +1,12 @@
+# hi angel and whomever else is watching my code
+# this is our garvie's movement
+# rpg-based.
+extends CharacterBody2D
+@export var speed = 400
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
